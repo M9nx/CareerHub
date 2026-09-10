22373accabd63965e12a7c1a2ee03a4162308137
diva-mariam <mariamdiva363@gmail.com>
2026-09-10 07:08:24 -0700

    feat(#11): fix UserRole import in factory and maintain test file

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
