dd4b3986879ed11d22c07b3ac8575ba22c768253
M9nx <flowmoner@gmail.com>
2026-09-13 09:59:09 +0300

    feat(#84): compose landing page from section components

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,15 @@
+<x-landing.layout>
+    <x-landing.loader />
+
+    <x-landing.header />
+
+    <main id="main">
+        <x-landing.hero />
+        <x-landing.stats />
+        <x-landing.features-bento />
+        <x-landing.showcase />
+        <x-landing.workflow />
+    </main>
+
+    <x-landing.footer />
+</x-landing.layout>
